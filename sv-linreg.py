0a0daf2fc8d7179a58e5e4002e5b9b8e8bb72699
ashraymallesh <ashraymallesh23@gmail.com>
2019-05-14 23:48:53 +0530

    fixed some things

--- sv-linreg.py
+++ sv-linreg.py
@@ -8,22 +8,6 @@ import matplotlib.pyplot as plt
 i_n = 0 # number of features
 i_m = 0 # number of training examples
 
-def computeCost(aX, v_y, vTheta):
-    # calculate cost for a given theta vector
-    sum = 0 # summation term in cost function
-    for i in range(1,i_m): 
-        sum += (h(vTheta, aX[i,:]) - v_y[i])**2
-    fCost = (1/(2*i_m))*sum
-    return fCost
-
-def h(vTheta, v_x):
-    # hypothesis function on a vector x
-    return np.dot(vTheta,v_x)
-
-def gradientDescent():
-    vTheta = np.zeros((2,1))
-    return vTheta
-
 def loadData(sCSVFilename):
     global i_m, i_n
     aData = np.loadtxt(sCSVFilename, delimiter=',')
@@ -35,8 +19,24 @@ def loadData(sCSVFilename):
     i_n = aX.shape[1] # (num features)
 
     plt.scatter(v_x, v_y) # scatterplot using first,second cols of aData as x,y
-    plt.show() 
+    plt.show()
 
     return aX, v_y
+
+def computeCost(aX, v_y, vTheta):
+    # calculate cost for a given theta vector
+    sum = 0 # summation term in cost function
+    for i in range(1,i_m): 
+        sum += (h(vTheta, aX[i,:].reshape(-1,1)) - v_y[i])**2
+    fCost = (1/(2*i_m))*sum
+    return fCost
+
+def h(vTheta, v_x):
+    # hypothesis function on a vector x
+    return np.dot(vTheta,v_x)
+
+def gradientDescent():
+    vTheta = np.zeros((2,1))
+    return vTheta
 
 aX, v_y = loadData('ex1data1.txt')
