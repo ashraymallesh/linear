914f3d2c965e26df57166300639fc63f99862529
ashraymallesh <ashraymallesh23@gmail.com>
2019-06-12 13:04:40 +0530

    cleaned up my code

--- sv-linreg.py
+++ sv-linreg.py
@@ -1,29 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-aData = np.genfromtxt('dataset.csv', delimiter=',')
-x = aData[1:,0].reshape(-1,1)
-y = aData[1:,-1].reshape(-1,1)
-m = x.size # number of training examples
-n = 2 # number of features
-ones = np.ones((m, 1)) # m-dim vector filled with 1s
-X = np.concatenate((ones, x), 1) # create design matrix X by adding ones as left col (x_0 = 1)
-theta = np.zeros((n,1)) # theta vector with 2 zeroes
+aData = np.genfromtxt('dataset.csv', delimiter=',', skip_header=1)
+m,n = aData.shape
+x = aData[:,0].reshape(-1,1)
+y = aData[:,-1].reshape(-1,1)
+X = np.c_[np.ones((m,1)), aData[:,0]] # create design matrix X by adding ones as left col (x_0 = 1)
 
-#compute cost using vectorized numpy computation instead of using for-loops
-error = (X @ theta) - y # mxn @ nx1 -> mx1 (matrix multiplication)
-cost = np.sum(error**2)/(2*m)
+def hyp(x, theta):
+	if x.shape == theta.shape: return np.dot(x,theta)
+	else: return x @ theta # mxn @ nx1 -> mx1 (matrix multiplication)
 
-def gradientDescent(X, y, theta, alpha, iters):
+def cost(theta, x, y):
+	#compute cost using vectorized numpy computation instead of using for-loops
+	error = hyp(x, theta) - y 
+	return np.sum(error**2) / 2*m
+
+def gradientDescent(x, y, theta, alpha, iters):
     for i in range(iters):
-        error = (X @ theta) - y # mxn @ nx1 -> mx1 (matrix multiplication)/
-        theta = theta - ((alpha/m) * np.sum(error * X, axis=0))
+        error = hyp(x, theta) - y
+        theta = theta - ((alpha/m) * np.sum(error * x, axis=0))
     return theta[0]
 
-print('Initial cost with theta = [0;0] is {0}'.format(cost))
+def plot():
+	global x,y,theta
+	plt.scatter(x,y) #original points scatterplot
+	y_pred = (theta[0]*x + theta[1])
+	plt.plot(x, y_pred, 'r') #plot regression line
+	plt.show()
 
+theta = np.zeros((n,1)) # theta vector with 2 zeroes
 theta = gradientDescent(X, y, theta, 0.001, 1000)
-plt.scatter(x,y) #original points scatterplot
-y_pred = (theta[0]*x + theta[1])
-plt.plot(x, y_pred, 'r') #plot regression line
-plt.show()
