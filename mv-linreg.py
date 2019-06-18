8c004025d25a0126dfd3f999942a381ff59965c8
ashraymallesh <ashraymallesh23@gmail.com>
2019-06-18 12:13:27 +0530

    add hyp and cost fn

--- mv-linreg.py
+++ mv-linreg.py
@@ -3,8 +3,20 @@ import matplotlib.pyplot as plt
 
 aData = np.genfromtxt('ex1data2.txt', delimiter=',', skip_header=1)
 m,n = aData.shape
-X = aData[:,0:2]
+X = aData[:,0:-1]
 y = aData[:,-1].reshape(-1,1)
-mean = np.mean(X, axis=0); std = np.std(X, axis=0)
+mean = np.mean(X, axis=0); std = np.std(X, axis=0) #store mean & std for each feature (col)
 X = (X - mean)/std #column wise feature normalization
 X = np.c_[np.ones(m), X]
+theta = np.zeros((n,1))
+
+def hyp(theta, X):
+	return np.dot(X, theta)
+
+def cost(theta, X, y):
+	error = hyp(theta, X) - y
+	return np.sum(error**2) / (2*m)
+
+def gradientDescent(theta, X, y, alpha, iters):
+	for _ in range(iters):
+		  
--- sv-linreg.py
+++ sv-linreg.py
@@ -7,29 +7,26 @@ x = aData[:,0].reshape(-1,1)
 y = aData[:,-1].reshape(-1,1)
 X = np.c_[np.ones((m,1)), aData[:,0]] # create design matrix X by adding ones as left col (x_0 = 1)
 
-def hyp(X, theta):
-	if X.shape == theta.shape:
-		return np.dot(X,theta)
-	else:
-		return X @ theta # mxn @ nx1 --> mx1
+def hyp(theta, X):
+	return np.dot(X,theta)
 
 def cost(theta, X, y):
 	#compute cost using vectorized numpy computation instead of for loops
-	error = hyp(X, theta) - y 
-	return np.sum(np.power(error, 2)) / (2*m)
+	error = hyp(theta, X) - y 
+	return np.sum(error**2) / (2*m)
 
-def gradientDescent(X, y, theta, alpha, iters):
+def gradientDescent(theta, X, y, alpha, iters):
     for _ in range(iters):
-        error = hyp(X, theta) - y
+        error = hyp(theta, X) - y
         theta = theta - ((alpha/m) * np.dot(X.T, error))
     return theta
 
 def plot(theta, X, y):
 	plt.scatter(X[:,1],y) #original points scatterplot
-	plt.plot(X[:,1], hyp(X, theta), 'r') #plot regression line
+	plt.plot(X[:,1], hyp(theta, X), 'r') #plot regression line
 	plt.show()
 
 theta = np.zeros((n,1)) # theta vector with 2 zeroes
 alpha = 0.01; iters = 1500
-theta = gradientDescent(X, y, theta, alpha, iters)
+theta = gradientDescent(theta, X, y, alpha, iters)
 plot(theta, X, y)
